0e55cdfe80b92330eb7860b49f985886428ba439
Karan Sharma <karan.sharma1@metacube.com>
2018-02-03 11:33:12 +0530

    OOPS Session 1 Assignment

---
+++ OOPS-session1/assignment_1/Div.java
@@ -0,0 +1,24 @@
+
+/**
+ * Div is a HTML tag
+ * Div is type of Composite Element
+ * 
+ * @author Karan Sharma
+ */
+package assignment_1;
+
+public class Div extends CompositeElement 
+{
+	
+	/**
+	 * Constructor to initialize the values
+	 * 
+	 * @param id
+	 * @param className
+	 */
+	Div(String id, String className)
+	{
+		// calling parent class for initialization
+		super(id,className);
+	}
+}
---
+++ OOPS-session1/assignment_1/Span.java
@@ -0,0 +1,23 @@
+
+/**
+ * Span is a HTML tag
+ * Span is type of Composite Element
+ * 
+ * @author Karan Sharma
+ */
+package assignment_1;
+
+public class Span extends CompositeElement
+{
+	/**
+	 * Constructor to initialize the values
+	 * 
+	 * @param id
+	 * @param className
+	 */
+	Span(String id, String className)
+	{
+		// calling parent class for initialization
+		super(id , className);
+	}
+}
