a7a5e3a99ed1404f97f28590b8422c51b7d6e0a7
meta-karan-sharma <35574932+meta-karan-sharma@users.noreply.github.com>
2018-03-06 12:12:18 +0530

    Design Pattern Session 1 Assignment

---
+++ DS-Session 1/com/metacube/shoppingcart/dao/BaseDao.java
@@ -0,0 +1,9 @@
+package com.metacube.shoppingcart.dao;
+
+/**
+ * this interface will used for getting the instance of storage type 
+ * @author Karan Sharma
+ */
+public interface BaseDao {
+	
+}
---
+++ DS-Session 1/com/metacube/shoppingcart/dao/DatabaseEnum.java
@@ -0,0 +1,10 @@
+package com.metacube.shoppingcart.dao;
+
+/**
+ * this enum contains storage type
+ * @author Karan Sharma
+ *
+ */
+public enum DatabaseEnum {
+	IN_MEMORY , SQL;
+}
